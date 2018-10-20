b3bc270b2badc7e137558d21410bef249211f90b
Tyler Jaacks <TylerJaacks@Gmail.com>
2018-10-19 21:53:03 -0500

    Added some drawing stuff.

--- static/game.js
+++ static/game.js
@@ -3,3 +3,12 @@ var socket = io();
 socket.on('message', function(data) {
   console.log(data);
 });
+
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.lineWidth="6";
+ctx.strokeStyle="red";
+ctx.rect(5,5,10,10); 
+ctx.stroke();
